68203a3d1cf6daf54f858017eb0daced3decad4d
unknown <Test@OB-USER-2103.onebanc.ai>
2021-09-29 19:52:16 +0530

    update0.1

--- app/src/main/java/com/example/restaurantapp/adapter/SegmentTwoAdapter.java
+++ app/src/main/java/com/example/restaurantapp/adapter/SegmentTwoAdapter.java
@@ -10,26 +10,52 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.restaurantapp.R;
+import com.example.restaurantapp.dataclass.Segments;
+
+import java.util.ArrayList;
 
 public class SegmentTwoAdapter extends RecyclerView.Adapter<SegmentTwoAdapter.ViewHolder> {
+    ArrayList<Segments> item;
+
+    public SegmentTwoAdapter(ArrayList<Segments> item) {
+        this.item = item;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View listItem= layoutInflater.inflate(R.layout.item_segment_two, parent, false);
-
-        SegmentOneAdpter.ViewHolder viewHolder2 = new SegmentOneAdpter.ViewHolder(listItem);
-        return viewHolder2;
+        SegmentTwoAdapter.ViewHolder viewHolder=new SegmentTwoAdapter.ViewHolder(listItem);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Segments data= item.get(position);
+        holder.tvSegmentTwo.setText(data.getName());
+        holder.tvSegmentTwo.setBackgroundResource(data.getImage());
+        holder.llSegmentTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
 
     }
 
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    public ArrayList<Segments> getItem() {
+        return item;
+    }
+
+    public void setItem(ArrayList<Segments> item) {
+        this.item = item;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
